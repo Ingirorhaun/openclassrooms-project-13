2f78fffbdf2b908f60281807b296b135f9e0af28
Ingirorhaun <francescopantusa@gmail.com>
2024-12-23 22:17:48 +0100

    update server config

--- server/server.js
+++ server/server.js
@@ -5,10 +5,14 @@ const swaggerUi = require('swagger-ui-express')
 const yaml = require('yamljs')
 const swaggerDocs = yaml.load('./swagger.yaml')
 const dbConnection = require('./database/connection')
+const path = require('path')
+
 
 dotEnv.config()
 
 const app = express()
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
 const PORT = process.env.PORT || 3001
 
 // Connect to the database
@@ -29,9 +33,10 @@ if (process.env.NODE_ENV !== 'production') {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 }
 
-app.get('/', (req, res, next) => {
-  res.send('Hello from my Express server v2!')
-})
+// Handle React routing, return all requests to React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'))
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`)

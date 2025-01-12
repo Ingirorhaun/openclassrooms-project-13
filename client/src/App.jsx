7adb0c912b05331e9bec0ab7f490a142f3212d5a
Ingirorhaun <francescopantusa@gmail.com>
2025-01-12 13:59:24 +0100

    check token validity and fetch user profile on app load

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,41 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getUserProfile } from "./api";
+import { setUserProfile } from "./store/features/userSlice";
+import { setToken } from "./store/features/authSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      // Fetch user profile with the stored token
+      const fetchUserProfile = async () => {
+        try {
+          const userProfile = await getUserProfile(token);
+          if (userProfile) {
+            dispatch(setUserProfile(userProfile));
+            dispatch(setToken(token));
+          }
+        } catch (error) {
+          console.error(error)
+          // Delete existing token on error
+          dispatch(setToken(null));
+          localStorage.removeItem("token");
+        }
+      };
+      fetchUserProfile();
+    }
+  }, [dispatch]);
+
   return (
     <>
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      <Outlet />
       <Footer />
     </>
   );

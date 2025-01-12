8afe47a000c171d3ce0addd5eb76a684a6c7e26d
Ingirorhaun <francescopantusa@gmail.com>
2025-01-12 13:57:34 +0100

    add logout functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,58 @@
 import ArgentBankLogo from "../assets/images/argentBankLogo.png";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from 'react-router-dom';
+import { logout } from '../store/features/authSlice';
+import { clearUserProfile } from "../store/features/userSlice";
 
 export const Header = () => {
+  const user = useSelector((state) => state.user);
+
+  const LogoutButton = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+    const handleLogout = async () => {
+      await Promise.all([
+        dispatch(clearUserProfile()),
+        dispatch(logout())
+      ]);
+      navigate("/");
+    };
+
+    return isAuthenticated ? (
+      <button className="logout-button main-nav-item" onClick={handleLogout}>
+        <i className="fa fa-sign-out"></i>
+        Sign Out
+      </button>
+    ) : null;
+  };
+
   return (
     <header>
       <nav className="main-nav">
-        <a className="main-nav-logo" href="./index.html">
+        <Link to="/" className="main-nav-logo">
           <img
             className="main-nav-logo-image"
             src={ArgentBankLogo}
             alt="Argent Bank Logo"
           />
           <h1 className="sr-only">Argent Bank</h1>
-        </a>
+        </Link>
         <div>
-          <a className="main-nav-item" href="./sign-in.html">
-            <i className="fa fa-user-circle"></i>
-            Sign In
-          </a>
+          {user.firstName ? (
+            <Link to="/profile" className="main-nav-item">
+              <i className="fa fa-user-circle"></i>
+              {user.firstName}
+            </Link>
+          ) : (
+            <Link to="/login" className="main-nav-item">
+              <i className="fa fa-user-circle"></i>
+              Sign In
+            </Link>
+          )}
+          <LogoutButton/>
         </div>
       </nav>
     </header>
--- client/src/store/features/authSlice.js
+++ client/src/store/features/authSlice.js
@@ -1,38 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  isAuthenticated: false,
-  token: null,
-  loading: false,
-  error: null,
+    isAuthenticated: false,
+    token: null,
+    loading: false,
+    error: null,
 };
 
 const authSlice = createSlice({
-  name: 'auth',
-  initialState,
-  reducers: {
-    loginStart: (state) => {
-      state.loading = true;
-      state.error = null;
+    name: 'auth',
+    initialState,
+    reducers: {
+        loginStart: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        setToken: (state, action) => {
+            state.isAuthenticated = true;
+            state.token = action.payload;
+            localStorage.setItem('token', action.payload);
+            state.loading = false;
+            state.error = null;
+        },
+        loginFailure: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
+        logout: (state) => {
+            state.isAuthenticated = false;
+            state.token = null;
+            localStorage.removeItem('token');
+            state.loading = false;
+            state.error = null;
+        },
     },
-    loginSuccess: (state, action) => {
-      state.isAuthenticated = true;
-      state.token = action.payload;
-      state.loading = false;
-      state.error = null;
-    },
-    loginFailure: (state, action) => {
-      state.loading = false;
-      state.error = action.payload;
-    },
-    logout: (state) => {
-      state.isAuthenticated = false;
-      state.token = null;
-      state.loading = false;
-      state.error = null;
-    },
-  },
 });
 
-export const { loginStart, loginSuccess, loginFailure, logout } = authSlice.actions;
+export const { loginStart, setToken, loginFailure, logout } = authSlice.actions;
 export default authSlice.reducer;
